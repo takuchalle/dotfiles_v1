6048dc6ca4a7cbd4d5c195eaaef01edfabeac06a
Takuya OHASHI <tky.ohashi@gmail.com>
2013-02-09 21:22:13 +0900

    設定ファイル反映スクリプト追加
     - install.sh 追加

---
+++ install.sh
@@ -0,0 +1,13 @@
+#/bin/zsh
+
+FILES=(".emacs.d" ".gitignore" ".tmux.conf" ".zshenv" ".zshrc" ".gemrc")
+PWD=`pwd`
+OLDPOSTFIX=".old"
+
+for file in ${FILES[@]}; do
+    if [ -e ~/$file -o -d ~/$file ]  
+    then
+	    mv ~/$file ~/$file$OLDPOSTFIX
+    fi
+    ln -sf $PWD/$file ~/$file
+done
