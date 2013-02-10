dad664f4411c8dd44eefd347e7d93d6bf8153463
Takuya OHASHI <tky.ohashi@gmail.com>
2013-02-10 13:56:45 +0900

    C/C++のコーディングスタイル変更
     - インデントの数を4に
     - tabは使用しない　
     - namespace内はインデントしない

--- .emacs.d/elisp/coding-style.el
+++ .emacs.d/elisp/coding-style.el
@@ -1,7 +1,17 @@
 ;;
-;; C Language
-(add-hook 'c-mode-common-hook
-          (lambda ()
-                 (c-set-style "bsd")))
+;; 
+(defun add-c-mode-conf ()
+  (c-set-style "cc-mode")
+  (setq indent-tabs-mode nil)
+  (setq c-basic-offset 4)
+  (c-set-offset 'innamespace 0)
+  )
 
+;;
+;; C Language
+(add-hook 'c-mode-common-hook 'add-c-mode-conf)
+
+;;
+;; C++ Language
+(add-hook 'c++-mode-common-hook 'add-c-mode-conf)
 
