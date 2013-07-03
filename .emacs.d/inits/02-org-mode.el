330879a38577f641c623f1c36cabefdbc9a26f7a
Takuya OHASHI <tky.ohashi@gmail.com>
2013-07-03 20:05:23 +0900

    org-mode の TODO 機能変更

--- .emacs.d/inits/02-org-mode.el
+++ .emacs.d/inits/02-org-mode.el
@@ -1,3 +1,12 @@
-(if (<= emacs-major-version 23)
-    (require 'org)
+(when (>= emacs-major-version 23)
+  (require 'org)
+  
+  ;; TODO
+  (setq org-use-fast-todo-selection t)
+  (setq org-todo-keywords
+	'((sequence "TODO(t)" "STARTED(s)" "WAIT(w)" "|" "DONE(d)""CANCEL(c)")
+	  (sequence "APPT(a)" "|" "DONE" "CANCEL(c)")))
+  (setq org-log-done 'time)
+
+  
   )
