c43c344173bc0537a2277213ecfc4a40385e4b4d
Takuya Ohashi <tky.ohashi@gmail.com>
2013-10-25 00:19:00 +0900

    [emacs] add epla setting
     only emacs version < 24

---
+++ .emacs.d/inits/00-elpa.el
@@ -0,0 +1,9 @@
+;; ELPA setting 
+(when (require 'package nil t)
+  (add-to-list 'package-archives
+	       '("marmalade" . "http://marmalade-repo.org/packages/"))
+  (add-to-list 'package-archives
+	       '("melpa" . "http://melpa.milkbox.net/packages/"))
+  (package-initialize)
+  )
+
