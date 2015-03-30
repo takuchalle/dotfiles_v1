9798fda6520718868286f0da78cb16029f1b9e9d
Takuya OHASHI <tky.ohashi@gmail.com>
2015-03-30 17:03:33 +0900

    [emacs] c/c++ coding style

--- .emacs.d/inits/10-c-lang-style.el
+++ .emacs.d/inits/10-c-lang-style.el
@@ -1,54 +1,57 @@
-;;
-;; 
-(defun add-c-mode-conf ()
-  (c-set-style "cc-mode")
-  (setq indent-tabs-mode t) ; ハードタブ
-  (setq c-basic-offset 8) ;; + のインデント幅を8にする
+(when (require 'cc-mode nil t)
 
-  ;; extern "C" { *** }
-  (c-set-offset 'extern-lang-open 0)
-  (c-set-offset 'inextern-lang 0)
-  (c-set-offset 'extern-lang-close 0)
+  ;;
+  ;; 
+  (defun add-c-mode-conf ()
+    (c-set-style "cc-mode")
+    (setq indent-tabs-mode t) ; ハードタブ
+    (setq c-basic-offset 8) ;; + のインデント幅を8にする
 
-  ;; ラベル
-  ;; *** :
-  (c-set-offset 'label 0)
+    ;; extern "C" { *** }
+    (c-set-offset 'extern-lang-open 0)
+    (c-set-offset 'inextern-lang 0)
+    (c-set-offset 'extern-lang-close 0)
 
-  ;; 式の途中で改行した時
-  ;; hoge =
-  ;;   *** ;
-  (c-set-offset 'statement-cont '+)
+    ;; ラベル
+    ;; *** :
+    (c-set-offset 'label 0)
 
-  ;; 予約後など，C++のトップレベルの文字列の後
-  ;; int
-  ;;   ***
-  (c-set-offset 'topmost-intro 0)
-  (c-set-offset 'topmost-intro-cont '+)
+    ;; 式の途中で改行した時
+    ;; hoge =
+    ;;   *** ;
+    (c-set-offset 'statement-cont '+)
 
-  ;; 名前空間
-  ;; namespace { *** }
-  (c-set-offset 'namespace-open 0)
-  (c-set-offset 'innamespace '0)
-  (c-set-offset 'namespace-close 0)
+    ;; 予約後など，C++のトップレベルの文字列の後
+    ;; int
+    ;;   ***
+    (c-set-offset 'topmost-intro 0)
+    (c-set-offset 'topmost-intro-cont '+)
 
-  ;; 関数内部
-  ;; int main(void){***}
-  (c-set-offset 'defun-open 0)
-  (c-set-offset 'defun-block-intro '+)
-  (c-set-offset 'defun-close 0)
+    ;; 名前空間
+    ;; namespace { *** }
+    (c-set-offset 'namespace-open 0)
+    (c-set-offset 'innamespace '0)
+    (c-set-offset 'namespace-close 0)
 
-  ;; }で終わる式
-  ;; enum {***}; etc.
-  (c-set-offset 'block-open 0)
-  (c-set-offset 'statement-block-intro '+)
-  (c-set-offset 'block-close 0)
+    ;; 関数内部
+    ;; int main(void){***}
+    (c-set-offset 'defun-open 0)
+    (c-set-offset 'defun-block-intro '+)
+    (c-set-offset 'defun-close 0)
+
+    ;; }で終わる式
+    ;; enum {***}; etc.
+    (c-set-offset 'block-open 0)
+    (c-set-offset 'statement-block-intro '+)
+    (c-set-offset 'block-close 0)
+    )
+
+  ;;
+  ;; C Language
+  (add-hook 'c-mode-common-hook 'add-c-mode-conf)
+
+  ;;
+  ;; C++ Language
+  (add-hook 'c++-mode-common-hook 'add-c-mode-conf)
+  (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
   )
-
-;;
-;; C Language
-(add-hook 'c-mode-common-hook 'add-c-mode-conf)
-
-;;
-;; C++ Language
-(add-hook 'c++-mode-common-hook 'add-c-mode-conf)
-
