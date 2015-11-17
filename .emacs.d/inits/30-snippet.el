542ab99c831b9132afab5adaf15e3b50971dd89a
Takuya OHASHI <tky.ohashi@gmail.com>
2015-11-17 13:35:20 +0900

    delete error for snippet

--- .emacs.d/inits/30-snippet.el
+++ .emacs.d/inits/30-snippet.el
@@ -2,20 +2,17 @@
 ;; Emacs のバージョンが24以上の時に有効
 
 (when (>= emacs-major-version 24)
-  (add-to-list 'load-path
-	       (expand-file-name "~/.emacs.d/elpa/yasnippet-20131021.928"))
   (when (require 'yasnippet nil t)
     ;; ~/.emacs.d/にsnippetsというフォルダを作っておきましょう
     (setq yas-snippet-dirs
 	  '("~/.emacs.d/snippets" ;; 作成するスニペットはここに入る
-	    "~/.emacs.d/elpa/yasnippet-20131021.928/snippets" ;; 最初から入っていたスニペット(省略可能)
 	    ))
     (yas-global-mode 1)
 
     ;; 単語展開キーバインド (ver8.0から明記しないと機能しない)
     ;; (setqだとtermなどで干渉問題ありでした)
     ;; もちろんTAB以外でもOK 例えば "C-;"とか
-    (custom-set-variables '(yas-trigger-key "TAB"))
+    (custom-set-variables '(yas-trigger-key "C-;"))
 
     ;; 既存スニペットを挿入する
     (define-key yas-minor-mode-map (kbd "C-x i i") 'yas-insert-snippet)
