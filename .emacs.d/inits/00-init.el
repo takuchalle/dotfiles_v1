f52cc024150271d1e1a6190b57f838677a94c1dd
Takuya OHASHI <tky.ohashi@gmail.com>
2016-10-18 17:02:07 +0900

    [emacs] delete iswitchd setting

--- .emacs.d/inits/00-init.el
+++ .emacs.d/inits/00-init.el
@@ -119,22 +119,22 @@
 (global-set-key "\C-u" 'other-window)
 
 ;;============iswitchb-mode========================================
-;; バッファの切り替えを強化する
-(iswitchb-mode 1)
-;;; C-f, C-b, C-n, C-p で候補を切り替えることができるように。
-(add-hook 'iswitchb-define-mode-map-hook
-	        (lambda ()
-		  (define-key iswitchb-mode-map "\C-n" 'iswitchb-next-match)
-		  (define-key iswitchb-mode-map "\C-p" 'iswitchb-prev-match)
-		  (define-key iswitchb-mode-map "\C-f" 'iswitchb-next-match)
-		  (define-key iswitchb-mode-map "\C-b" 'iswitchb-prev-match)))
+;; ;; バッファの切り替えを強化する
+;; (iswitchb-mode 1)
+;; ;;; C-f, C-b, C-n, C-p で候補を切り替えることができるように。
+;; (add-hook 'iswitchb-define-mode-map-hook
+;; 	        (lambda ()
+;; 		  (define-key iswitchb-mode-map "\C-n" 'iswitchb-next-match)
+;; 		  (define-key iswitchb-mode-map "\C-p" 'iswitchb-prev-match)
+;; 		  (define-key iswitchb-mode-map "\C-f" 'iswitchb-next-match)
+;; 		  (define-key iswitchb-mode-map "\C-b" 'iswitchb-prev-match)))
 
-;; バッファ読み込み関数をiswitchb にする
-(setq read-buffer-function 'iswitchb-read-buffer)
-;; 部分文字列の代わりに正規表現を使う場合は t を設定する
-(setq iswitchb-regexp nil)
-;; 新しいバッファを作成するときにいちいち聞いてこない
-(setq iswitchb-prompt-newbuffer nil)
+;; ;; バッファ読み込み関数をiswitchb にする
+;; (setq read-buffer-function 'iswitchb-read-buffer)
+;; ;; 部分文字列の代わりに正規表現を使う場合は t を設定する
+;; (setq iswitchb-regexp nil)
+;; ;; 新しいバッファを作成するときにいちいち聞いてこない
+;; (setq iswitchb-prompt-newbuffer nil)
 ;;============iswitchb-mode=======================================
 
 
