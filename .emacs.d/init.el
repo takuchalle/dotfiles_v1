2c98075a20d51c50895f8b2a9e5697d89e8267d0
Takuya OHASHI <tky.ohashi@gmail.com>
2017-03-07 12:17:23 +0900

    [emacs] hi-light current cursor

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -131,3 +131,6 @@
 ;; http://emacs.rubikitch.com/mwim/
 (global-set-key (kbd "C-a") 'mwim-beginning-of-code-or-line)
 (global-set-key (kbd "C-e") 'mwim-end-of-code-or-line)
+
+;; hilight current cursor
+(global-hl-line-mode)
