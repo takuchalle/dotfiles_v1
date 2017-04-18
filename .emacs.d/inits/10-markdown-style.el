842920a6066d51d9a1e76e76e62f54965a585d42
Takuya OHASHI <tky.ohashi@gmail.com>
2017-04-18 14:28:12 +0900

    [emacs] change markdown section color
     - use org-mode section color

--- .emacs.d/inits/10-markdown-style.el
+++ .emacs.d/inits/10-markdown-style.el
@@ -2,3 +2,14 @@
   "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+(custom-set-faces
+ '(markdown-header-delimiter-face ((t (:inherit org-mode-line-clock))))
+ '(markdown-header-face-1 ((t (:inherit outline-1 :weight bold))))
+ '(markdown-header-face-2 ((t (:inherit outline-2 :weight bold))))
+ '(markdown-header-face-3 ((t (:inherit outline-3 :weight bold))))
+ '(markdown-header-face-4 ((t (:inherit outline-4 :weight bold))))
+ '(markdown-header-face-5 ((t (:inherit outline-5 :weight bold))))
+ '(markdown-header-face-6 ((t (:inherit outline-6 :weight bold))))
+ '(markdown-pre-face ((t (:inherit org-formula))))
+ )
